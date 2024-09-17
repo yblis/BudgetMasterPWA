1ce69320d48a0ea6e5d3aa77ba6050c37fd3ac2f
yblis <yblisss@yahoo.fr>
2024-09-17 05:33:26 +0000

    vapi

--- generate_vapid_keys.py
+++ generate_vapid_keys.py
@@ -1,10 +1,10 @@
-from py_vapid import Vapid01
+from py_vapid import Vapid
 
-vapid = Vapid01()
+vapid = Vapid()
 vapid.generate_keys()
 
-private_key = vapid.private_key.to_pem().decode('utf-8').strip()
-public_key = vapid.public_key.to_pem().decode('utf-8').strip()
+private_key = vapid.private_key.decode('utf-8').strip()
+public_key = vapid.public_key.decode('utf-8').strip()
 
 print(f"VAPID_PRIVATE_KEY = '{private_key}'")
 print(f"VAPID_PUBLIC_KEY = '{public_key}'")
